56f8bcdd2f80d66a2872378778712b709271300e
Harrison Melton <hmmelton95@gmail.com>
2016-10-28 23:00:18 -0400

    removed example Intent code

--- app/src/main/java/com/team24/bric/MainActivity.java
+++ app/src/main/java/com/team24/bric/MainActivity.java
@@ -1,8 +1,7 @@
 package com.team24_jpm.bric;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, SplashscreenActivity.class);
-        startActivity(intent);
     }
 }
