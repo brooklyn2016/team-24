eb44ba61e667ab86f315bf799e378dfc9475f44a
Robert Kump <kumprj@miamioh.edu>
2016-10-29 01:06:14 -0400

    File name problem

--- app/src/main/java/com/team24/bric/UploadActivity.java
+++ app/src/main/java/com/team24/bric/UploadActivity.java
@@ -3,6 +3,7 @@ package com.team24_jpm.bric;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,11 +17,14 @@ public class UploadActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.upload_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+
                 int reqCode = 1;
                 Intent action = new Intent(Intent.ACTION_GET_CONTENT);
                 action = action.setType("file/*").addCategory(Intent.CATEGORY_OPENABLE);
                 startActivityForResult(action, reqCode);
+                // Runs immediately, causing getPath to run on a Null object. Fix
+                //String s = action.getData().getPath();
+                //Log.d("Upload Activity", s);
             }
         });
 
