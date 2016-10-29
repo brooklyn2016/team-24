d56f936a4eb4a18991ae184403085c0fad440d31
Robert Kump <kumprj@miamioh.edu>
2016-10-29 04:45:10 -0400

    No firebase, FB Auth tho

--- app/src/main/java/com/team24/bric/LoginActivity.java
+++ app/src/main/java/com/team24/bric/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.team24_jpm.bric;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+
 }
