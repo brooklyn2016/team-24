e47e6a2c4e74821dcbfa63ae9250710e2fb6b1ab
Lily Nguyen <lily@nguyensomniac.com>
2016-10-29 05:07:09 -0400

    add zapier hook

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var firebase = require('firebase');
-
-// https://hooks.zapier.com/hooks/catch/1761861/679qd6/
+var request = require('request');
+var bodyParser = require('body-parser');
 
 // Initialize Firebase
 firebase.initializeApp({
@@ -10,4 +10,54 @@ firebase.initializeApp({
 });
 
 var db = firebase.database();
-console.log(db);
+
+var sendConfirmationEmail = function(submission) {
+
+  var email = submission.email;
+  // do nothing if user has no email or if email confirmation
+  // was already sent
+  if (submission.emailConfirmationSent || ! email) {
+    return;
+  }
+
+  var zapierHook = 'https://hooks.zapier.com/hooks/catch/1761861/679qd6/';
+  request({
+    url: zapierHook,
+    method: 'POST',
+    json: submission
+  })
+}
+
+var onNewComplete = function(snapshot) {
+  var val = snapshot.val();
+  var key = snapshot.key;
+  sendConfirmationEmail(val);
+  if (val.email && val.emailConfirmationSent) {
+    db.ref('submissions/' + key + '/emailConfirmationSent').set(true);
+  }
+}
+
+// Listen to database updates
+var submissionsRef = db.ref('submissions');
+submissionsRef.orderByChild('emailConfirmationSent').equalTo(null).on('child_changed', onNewComplete);
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send(200, 'Hello!')
+})
+
+// mock a request to the zapier webhook
+// @param {string} req.body.email - the user's email
+app.post('/send_email_confirmation', function(req, res) {
+  var body = req.body;
+  console.log(req)
+  if (!body || !body.email) {
+    res.sendStatus(400);
+  }
+  sendConfirmationEmail(body);
+  res.sendStatus(200);
+})
+
+app.listen(8000);
