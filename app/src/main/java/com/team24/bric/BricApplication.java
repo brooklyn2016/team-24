ca94d190b42160564280ea37e571ed5678c89a86
Harrison Melton <hmmelton@comcast.net>
2016-10-29 05:17:54 -0400

    finished storage file

--- app/src/main/java/com/team24/bric/BricApplication.java
+++ app/src/main/java/com/team24/bric/BricApplication.java
@@ -9,10 +9,23 @@ import com.facebook.FacebookSdk;
  */
 
 public class BricApplication extends Application {
+
+    private static BricApplication instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        instance = this;
+    }
+
+    /**
+     * This method is used to allow helper files access to Context.
+     * @return
+     */
+    public static BricApplication getInstance() {
+        return instance;
     }
 
 }
--- app/src/main/java/com/team24/bric/helpers/Storage.java
+++ app/src/main/java/com/team24/bric/helpers/Storage.java
@@ -1,9 +1,41 @@
 package com.team24_jpm.bric.helpers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.team24_jpm.bric.BricApplication;
+
 /**
  * Created by harrisonmelton on 10/29/16.
+ * Helper file for SharedPreferences
  */
 public class Storage {
 
+    private static final SharedPreferences PREFS =
+            BricApplication.getInstance().getSharedPreferences("bric_shared_prefs",
+                    Context.MODE_PRIVATE);
+    private static final SharedPreferences.Editor EDITOR = PREFS.edit();
 
+    /*
+    KEYS
+     */
+    private static final String LOGGED_IN_KEY = "logged_in";
+
+    /**
+     * Check if user is currently logged in.
+     * @return boolean representing whether or not user is currently logged in
+     */
+    public static boolean isLoggedIn() {
+        return PREFS.getBoolean(LOGGED_IN_KEY, false);
+    }
+
+    /**
+     * Logs user in/out
+     * @param loggedIn boolean representing whether the user should be logged in (true) or out
+     *                 (false)
+     */
+    public static void setLogin(boolean loggedIn) {
+        EDITOR.putBoolean(LOGGED_IN_KEY, loggedIn);
+        EDITOR.commit();
+    }
 }
